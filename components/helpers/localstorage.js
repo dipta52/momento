8978632ac2230023892665a9c0cf791e8648ffcb
Rohit Sah <sahrohit9586@gmail.com>
2022-07-31 00:59:49 +0530

    Docs init

--- components/helpers/localstorage.js
+++ components/helpers/localstorage.js
@@ -1,9 +1,18 @@
+/**
+ * Write the value to local storage with the key
+ * @param {string} key
+ * @param {string} value
+ */
 export const setToStorage = (key, value) => {
 	if (typeof window !== "undefined") {
 		return window.localStorage.setItem(key, value);
 	}
 };
 
+/**
+ * Get the value from local storage with the key
+ * @param {string} key
+ */
 export const getFromStorage = (key) => {
 	if (typeof window !== "undefined") {
 		return window.localStorage.getItem(key);
