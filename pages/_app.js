3f44840a087a9d3bf8863a97037947206f680c12
Rohit Sah <sahrohit9586@gmail.com>
2022-08-03 00:01:57 +0530

    material ui init

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,37 @@
-import "../styles/globals.css";
 import "nprogress/nprogress.css";
+import "../styles/globals.css";
 
+import { AuthProvider } from "@contexts/AuthContext";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import Head from "next/head";
 import Router from "next/router";
 import NProgress from "nprogress";
-import { AuthProvider } from "@contexts/AuthContext";
+import createEmotionCache from "@lib/createEmotionCache";
+import theme from "@config/theme";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-function MyApp({ Component, pageProps }) {
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
 	return (
-		<AuthProvider>
-			<Component {...pageProps} />;
-		</AuthProvider>
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<AuthProvider>
+					<Component {...pageProps} />;
+				</AuthProvider>
+			</ThemeProvider>
+		</CacheProvider>
 	);
 }
 
